b11950f96aa4db566cb428dfebb2f7fee65f8380
tjseorud <35774943+tjseorud@users.noreply.github.com>
2025-01-10 17:50:38 +0900

    JS-10-Q1

---
+++ 연습문제/옵션적용하기/옵션적용하기.js
@@ -0,0 +1,33 @@
+const btn =document.querySelector('#btn');  /* 적용버튼 */
+const inWidth =document.querySelector('#input-w');  /* 너비 */
+const inHeight =document.querySelector('#input-h'); /* 높이 */
+const fontSize =document.querySelector('#input-fs');  /* 글자크기 */
+// 처음 html 로딩시 고정되니 전역으로 사용 X
+// const fontW =document.querySelector('[name=input-fw:checked]'); /* 글자굵기 */
+const color =document.querySelector('#input-fc'); /* 글자색 */
+const bgc =document.querySelector('#input-bg'); /* 배경색 */
+// const fontJc =document.querySelector('[name=input-jc:checked]'); /* 가로정렬 */
+// const fontAi =document.querySelector('[name=input-ai:checked]'); /* 글자입력 */
+const inText =document.querySelector('#input-text');  /* 입력창 */
+const display =document.querySelector('#box');  /* 뷰 */
+btn.addEventListener('click',function(){
+  display.style.width =inWidth.value +'px'; /* 너비 */
+  display.style.height =inHeight.value +'px'; /* 높이 */
+  display.style.fontSize =fontSize.value +'px'; /* 글자크기 */
+  /* name 중 fb 인 요소중 체크가 되있는 요소 1개만 선택
+   * :checked -> 클릭된 요소만 선택 */
+  const fontW =document.querySelector('[name=input-fw:checked]'); /* 글자굵기 */
+  display.style.fontWeight =fontW.value;
+  
+  display.style.color =color.value;
+  display.style.backgroundColor =bgc.value;
+
+  for(let i=0;i<fontJc.length;i++){ /* 가로정렬 */
+    fontJc[i].addEventListener('change',function(e){display.style.justifyContent =e.target.value});
+  }
+  for(let i=0;i<fontAi.length;i++){ /* 세로정렬 */
+    fontAi[i].addEventListener('change',function(e){display.style.alignItems =e.target.value});
+  }
+  display.innerHTML =inText.value;
+  
+});
