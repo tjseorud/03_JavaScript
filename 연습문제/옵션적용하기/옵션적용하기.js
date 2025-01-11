4739a41f05f11b4fe7c8f9b8ec02dcd1daa9a864
tjseorud <35774943+tjseorud@users.noreply.github.com>
2025-01-11 13:33:28 +0900

    Update 옵션적용하기.js

--- 연습문제/옵션적용하기/옵션적용하기.js
+++ 연습문제/옵션적용하기/옵션적용하기.js
@@ -13,21 +13,17 @@ const display =document.querySelector('#box');  /* 뷰 */
 btn.addEventListener('click',function(){
   display.style.width =inWidth.value +'px'; /* 너비 */
   display.style.height =inHeight.value +'px'; /* 높이 */
-  display.style.fontSize =fontSize.value +'px'; /* 글자크기 */
+  display.style.fontSize =fontSize.value +'px'; /* 글자크기 */  
+  display.style.color =color.value;
+  display.style.backgroundColor =bgc.value;
+  display.innerText =inText.value;
+  
   /* name 중 fb 인 요소중 체크가 되있는 요소 1개만 선택
    * :checked -> 클릭된 요소만 선택 */
   const fontW =document.querySelector('[name=input-fw:checked]'); /* 글자굵기 */
   display.style.fontWeight =fontW.value;
-  
-  display.style.color =color.value;
-  display.style.backgroundColor =bgc.value;
-
-  for(let i=0;i<fontJc.length;i++){ /* 가로정렬 */
-    fontJc[i].addEventListener('change',function(e){display.style.justifyContent =e.target.value});
-  }
-  for(let i=0;i<fontAi.length;i++){ /* 세로정렬 */
-    fontAi[i].addEventListener('change',function(e){display.style.alignItems =e.target.value});
-  }
-  display.innerHTML =inText.value;
-  
+  const fontJc =document.querySelector('[name=input-jc:checked]'); /* 가로정렬 */
+  display.style.justifyContent =fontJc.value;
+  const fontAi =document.querySelector('[name=input-ai:checked]'); /* 세로정렬 */
+  display.style.alignItems =fontAi.value;
 });
